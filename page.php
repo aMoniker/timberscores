76a24b87a658ba70869ad00a48330b314b3d30e7
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-05-25 16:39:51 -0400

    page template

--- page.php
+++ page.php
@@ -10,26 +10,9 @@
  * @package timberscores
  */
 
-get_header(); ?>
+$context = timberscores_get_context();
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+$context['posts'] = Timber::get_posts();
+$context['sidebar'] = Timber::get_sidebar();
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+timberscores_render_page('views/page.twig', $context);
