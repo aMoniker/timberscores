1fb48969f31515822e260fbce375e5f6d78c0616
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-05-25 18:04:16 -0400

    sidebar template

--- sidebar.php
+++ sidebar.php
@@ -4,29 +4,12 @@
  *
  * @package timberscores
  */
+
+$context = timberscores_get_context();
+
+$context['widgets_sidebar_1'] = Timber::get_widgets('sidebar-1');
+$context['widgets_sidebar_1_has_widgets'] = is_active_sidebar('sidebar-1');
+
+Timber::render('views/sidebar.twig', $context);
+
 ?>
-	<div id="secondary" class="widget-area" role="complementary">
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-			<aside id="search" class="widget widget_search">
-				<?php get_search_form(); ?>
-			</aside>
-
-			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'timberscores' ); ?></h1>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
-			</aside>
-
-			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'timberscores' ); ?></h1>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</aside>
-
-		<?php endif; // end sidebar widget area ?>
-	</div><!-- #secondary -->
