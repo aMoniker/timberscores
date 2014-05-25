df5fd705270405f9d90dc8807e96708b4306ae66
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-05-25 17:53:47 -0400

    search template

--- search.php
+++ search.php
@@ -5,41 +5,10 @@
  * @package timberscores
  */
 
-get_header(); ?>
+$context = timberscores_get_context();
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+$context['posts'] = Timber::get_posts();
 
-		<?php if ( have_posts() ) : ?>
+timberscores_render_page('views/search.twig', $context);
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'timberscores' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'content', 'search' );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php timberscores_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+?>
