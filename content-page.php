5f44ab150ef8d857f2ace1a8c5e533488a090540
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-05-25 17:03:54 -0400

    content-page template part

--- content-page.php
+++ content-page.php
@@ -4,23 +4,14 @@
  *
  * @package timberscores
  */
+
+global $timber_template_part_context;
+if (!$timber_template_part_context) { // TODO: This is ugly.. need a better way
+	$timber_template_part_context = array();
+}
+
+$context = array_merge(timberscores_get_context(), $timber_template_part_context);
+
+Timber::render('views/content-page.twig', $context);
+
 ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'timberscores' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'timberscores' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
