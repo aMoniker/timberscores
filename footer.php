004db6c0107bc7734f42946155a71126c243aeab
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-05-25 17:32:33 -0400

    footer template

--- footer.php
+++ footer.php
@@ -6,20 +6,9 @@
  *
  * @package timberscores
  */
+
+$context = timberscores_get_context();
+
+Timber::render('views/footer.twig', $context);
+
 ?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'timberscores' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'timberscores' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'timberscores' ), 'timberscores', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
